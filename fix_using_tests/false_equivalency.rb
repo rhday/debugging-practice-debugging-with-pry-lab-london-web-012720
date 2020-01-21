3f1adc831a98ac0d0c85a0197d116405f741e0f1
Ruari Day <ruariday@gmail.com>
2020-01-21 11:52:47 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -12,10 +12,10 @@ end
 def selection(num)
   if num = 1
     if num == 1
-     "YUM YUM MUNCH MUNCH MUNCH"
+     return "YUM YUM MUNCH MUNCH MUNCH"
   elsif num = 2
   elsif num == 2
-     "HAM HAM HAM IN MY TUMMY"
+     return "HAM HAM HAM IN MY TUMMY"
   end
   num
 end
