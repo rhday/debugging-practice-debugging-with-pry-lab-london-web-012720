add8d83dd6ae8036991263659b08d9a2b48723eb
Ruari Day <ruariday@gmail.com>
2020-01-21 11:54:59 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -15,12 +15,13 @@ def selection(num)
       "YUM YUM MUNCH MUNCH MUNCH"
   elsif num = 2
   elsif num == 2
-     return "HAM HAM HAM IN MY TUMMY"
+      "HAM HAM HAM IN MY TUMMY"
   end
   num
 end
 def runner
   prompt_user
   selection(get_user_input)
+  binding.pry
 end
 end
