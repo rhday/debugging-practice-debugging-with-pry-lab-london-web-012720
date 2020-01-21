d5c71926fc85ceb8e3ac4e5d1f0170dd96490998
Ruari Day <ruariday@gmail.com>
2020-01-21 11:38:47 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -1,9 +1,7 @@
 
-
 def get_user_input
   gets.chomp
 end
-
 def prompt_user
   puts "What would you like to do?"
   puts "1.) Eat a hamburger."
@@ -12,12 +10,13 @@ end
 
 def selection(num)
   if num = 1
+  if num == 1
      "YUM YUM MUNCH MUNCH MUNCH"
   elsif num = 2
+  elsif num == 2
      "HAM HAM HAM IN MY TUMMY"
   end
 end
-
 def runner
   prompt_user
   selection(get_user_input)
