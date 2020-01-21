2d2690b8fcbd61703c56e179253eaa475b47adeb
Ruari Day <ruariday@gmail.com>
2020-01-21 11:37:14 +0000

    Automatically backed up by Learn

--- fix_using_tests/classical_gas.rb
+++ fix_using_tests/classical_gas.rb
@@ -1,22 +1,14 @@
-require 'pry'
-
 class GasStation
 
-  # Remember which methods this "magic command", "attr_reader" creates.
-  # It writes "get" for these two instance variables.
-  # https://www.rubyguides.com/2018/11/attr_accessor/
-  # https://stackoverflow.com/questions/20018784/attr-accessor-vs-attr-reader-instance-variables/20018831
   attr_reader :brand, :unleaded_price
+  attr_accessor :unleaded_price
+  attr_reader :brand
 
   def initialize(brand, unleaded_price)
     @brand = brand
     @unleaded_price = unleaded_price
-    
-  
   end
-
 end
-
-dinobones = GasStation.new("Dino Bones Gas and Grill", 100)
 petrol_petes = GasStation.new("Petrol Pete's", 50)
 seashore_shell = GasStation.new("The Seashore Shell", 40)
+dinobones = GasStation.new("Dino Bones Gas and Grill", 60)
